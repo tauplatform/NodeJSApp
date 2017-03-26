c973b1d65387b02e742516e26ad0dcabe971719a
Dmitry Soldatenkov <dsoldatenkov@tau-technologies.com>
2017-03-27 00:59:19 +0300

    add application code

---
+++ nodejs/server/app.js
@@ -0,0 +1,25 @@
+
+var server_port = Rho.System.NodejsServerPort
+
+var path = require('path');
+var express = require('express');
+var app = express();
+
+global.myfunc = function() {
+    Rho.Log.info("$$$$$$$$$$$$$$$$$ RUN NODEJS CODE !!!", 'Node.js JS');
+    Rho.WebView.executeJavascript("showAlert();");
+};
+
+
+app.use('/public', express.static(path.join(__dirname, 'public')));
+
+app.get('/', function (req, res) {
+  res.send('Hello World! (' + Date.now() + ")");
+});
+
+var server = app.listen(server_port, function () {
+  Rho.Log.info("Express server is started. (port: "+server_port+")", "Node.js JS");
+  // application must be inform RHomobile platform about starting of http server !
+  Mobile.httpServerStarted();
+});
+
